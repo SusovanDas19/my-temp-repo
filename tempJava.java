8cdc2674705a917ecec79efe350493b3f54f6504
Susovan Das <amesusovan@gmail.com>
2026-03-12 16:06:09 +0530

    Added Java  program for Jenkins CI/CD

--- tempJava.java
+++ tempJava.java
@@ -1,5 +1,5 @@
 public class tempJava {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD Pipeline!");
+        System.out.println("Hello from Jenkins CI/CD Pipeline");
     }
 }
